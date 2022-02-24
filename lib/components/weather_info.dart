52dfa17e47350208437b8774b6fff418ca2218d5
GLinBoy <hojjatabedie@gmail.com>
2022-02-24 14:14:39 +0330

    👌 IMPROVE: Put a card around weather info

--- lib/components/weather_info.dart
+++ lib/components/weather_info.dart
@@ -16,16 +16,28 @@ class WeatherInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset(
-          imagePath,
-          width: 64,
-          height: 64,
-        ),
-        Text(
-          '$degree°',
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  imagePath,
+                  width: 64,
+                  height: 64,
+                ),
+                Text(
+                  '$degree°',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
         const SizedBox(height: 7.0),
