33330ebf1a99dee4a25eeea7f59274d955d68ffd
GLinBoy <hojjatabedie@gmail.com>
2022-02-24 11:20:22 +0330

    📦 NEW: Added a weather info component

---
+++ lib/components/weather_info.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class WeatherInfo extends StatelessWidget {
+  final String imagePath;
+  final String degree;
+  final String dayName;
+
+  const WeatherInfo({
+    Key? key,
+    required this.imagePath,
+    required this.degree,
+    required this.dayName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          imagePath,
+          width: 64,
+          height: 64,
+        ),
+        Text(
+          '$degree°',
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 14.0,
+        ),
+        Text(dayName),
+      ],
+    );
+  }
+}
