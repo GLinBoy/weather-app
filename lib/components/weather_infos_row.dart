80557559b93e293fcac0facca0904eded6ba1026
GLinBoy <hojjatabedie@gmail.com>
2022-02-24 11:33:55 +0330

    👌 IMPROVE: Added horizental scroll to weatherinfo

--- lib/components/weather_infos_row.dart
+++ lib/components/weather_infos_row.dart
@@ -6,45 +6,48 @@ class WeatherInfosRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: const [
-        WeatherInfo(
-          imagePath: 'assets/images/cloud.png',
-          degree: '22',
-          dayName: 'Sunday',
-        ),
-        WeatherInfo(
-          imagePath: 'assets/images/cloudy-day.png',
-          degree: '20',
-          dayName: 'Monday',
-        ),
-        WeatherInfo(
-          imagePath: 'assets/images/cloudy-night.png',
-          degree: '22',
-          dayName: 'Tuesday',
-        ),
-        WeatherInfo(
-          imagePath: 'assets/images/cloudy.png',
-          degree: '20',
-          dayName: 'Wendsday',
-        ),
-        WeatherInfo(
-          imagePath: 'assets/images/rain.png',
-          degree: '18',
-          dayName: 'Tuersday',
-        ),
-        WeatherInfo(
-          imagePath: 'assets/images/snowy.png',
-          degree: '16',
-          dayName: 'Friday',
-        ),
-        WeatherInfo(
-          imagePath: 'images/storm.png',
-          degree: '14',
-          dayName: 'Satarday',
-        ),
-      ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          WeatherInfo(
+            imagePath: 'assets/images/cloud.png',
+            degree: '22',
+            dayName: 'Sunday',
+          ),
+          WeatherInfo(
+            imagePath: 'assets/images/cloudy-day.png',
+            degree: '20',
+            dayName: 'Monday',
+          ),
+          WeatherInfo(
+            imagePath: 'assets/images/cloudy-night.png',
+            degree: '22',
+            dayName: 'Tuesday',
+          ),
+          WeatherInfo(
+            imagePath: 'assets/images/cloudy.png',
+            degree: '20',
+            dayName: 'Wendsday',
+          ),
+          WeatherInfo(
+            imagePath: 'assets/images/rain.png',
+            degree: '18',
+            dayName: 'Tuersday',
+          ),
+          WeatherInfo(
+            imagePath: 'assets/images/snowy.png',
+            degree: '16',
+            dayName: 'Friday',
+          ),
+          WeatherInfo(
+            imagePath: 'assets/images/storm.png',
+            degree: '14',
+            dayName: 'Satarday',
+          ),
+        ],
+      ),
     );
   }
 }
