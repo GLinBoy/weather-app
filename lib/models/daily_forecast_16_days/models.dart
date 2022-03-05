ceb3e0eb80d1601c70166a3950ebe3fdd44f03e2
GLinBoy <hojjatabedie@gmail.com>
2022-03-05 14:42:59 +0330

    📦 NEW: Added models files of Daily Forecast 16 days API
    resolved #23

---
+++ lib/models/daily_forecast_16_days/models.dart
@@ -0,0 +1,8 @@
+export 'daily_forecast_16_days_response.dart';
+export 'city.dart';
+export 'coordinate.dart';
+export 'models.dart';
+export 'weather_data.dart';
+export 'weather_feels_like.dart';
+export 'weather_data_info.dart';
+export 'weather_data_temp.dart';
